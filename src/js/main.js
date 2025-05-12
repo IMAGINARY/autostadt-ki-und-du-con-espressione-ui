83c2149d8eaae1ce4cc500cde76548e41903af32
Christian Stussak <christian.stussak@imaginary.org>
2025-05-12 17:03:12 +0200

    Set particle coloring to `autostadt-ki-und-du-tempo-loudness-ml-impact`

--- src/js/main.js
+++ src/js/main.js
@@ -180,7 +180,7 @@
             velocity: new THREE.Vector3(),
             velocityRandomness: 1,
             color: "#ff1493",
-            particleColoring: 'autostadt-ki-und-du-time-ml',
+            particleColoring: 'autostadt-ki-und-du-tempo-loudness-ml-impact',
             colorRandomness: .2,
             turbulence: 1.2,
             lifetime: 6,
